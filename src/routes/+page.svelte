6c483c2ab8b7d5072de2721bbd2b6d0e59827089
Merkie <merkie@post.com>
2023-09-19 19:55:55 -0500

    add a bunch of ui stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,37 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { fly, scale } from 'svelte/transition';
 
 	export let data;
+
+	let results = data.initialResults;
 
 	let query = '';
 	let page = 0;
 
-	let results = data.initialResults;
-
 	let loadingMoreResults = false;
 	let finishedLoading = false;
+
+	let filterResults = true;
+	let providersFilterOpen = false;
+
+	let providers = [
+		{
+			name: 'Arasaac',
+			id: 'arasaac',
+			enabled: true
+		},
+		{
+			name: 'Mulberry',
+			id: 'mulberry',
+			enabled: true
+		},
+		{
+			name: 'Sclera Picto',
+			id: 'picto',
+			enabled: true
+		}
+	];
 
 	const resultToUrl = (result: any) => {
 		if (result.provider === 'mulberry')
@@ -44,6 +66,14 @@
 			}
 		});
 	});
+
+	$: results = results.filter((result: any) => {
+		if (filterResults) {
+			if (result.violence || result.sex) return false;
+		}
+		if (!providers.find((provider) => provider.id === result.provider)?.enabled) return false;
+		return true;
+	});
 </script>
 
 <div class="p-4 flex gap-4">
@@ -64,6 +94,71 @@
 			type="text"
 			class="p-2 outline-none flex-1 bg-transparent"
 		/>
+	</div>
+	<div class="relative">
+		<button
+			on:click={() => (providersFilterOpen = !providersFilterOpen)}
+			class="bg-zinc-50 border border-zinc-300 shadow-sm flex rounded-md p-2 px-4"
+		>
+			<p class="pr-16">
+				{providers.filter((p) => p.enabled).length === providers.length
+					? 'All Providers'
+					: // if all providers are disabled
+					providers.filter((p) => p.enabled).length === 0
+					? 'No Providers'
+					: // if some providers are enabled
+					  'Providers: ' +
+					  providers
+							.filter((p) => p.enabled)
+							.map((p) => p.name)
+							.join(', ')}
+			</p>
+			<i class="bi bi-chevron-down" />
+		</button>
+		{#if providersFilterOpen}
+			<div
+				transition:fly={{ y: 20, duration: 200 }}
+				class="bg-zinc-50 w-[250px] border border-zinc-300 shadow-sm flex flex-col rounded-md p-2 absolute -bottom-[10px] translate-y-[100%] right-0"
+			>
+				{#each providers as provider}
+					<button
+						on:click={() => (provider.enabled = !provider.enabled)}
+						class="flex gap-2 items-center p-2 justify-between"
+					>
+						<p>{provider.name}</p>
+						<div
+							class={`w-[20px] h-[20px] text-blue-50 relative rounded-md border-2 transition-all ${
+								provider.enabled ? 'bg-blue-500 border-transparent' : ' border-zinc-500'
+							}`}
+						>
+							{#if provider.enabled}
+								<i
+									transition:scale
+									class="bi bi-check text-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+								/>
+							{/if}
+						</div>
+					</button>
+				{/each}
+			</div>
+		{/if}
+	</div>
+	<div class="flex">
+		<button on:click={() => (filterResults = !filterResults)} class="flex gap-2 items-center">
+			<p>Filter Nudity and Violence</p>
+			<div
+				class={`w-[20px] h-[20px] text-blue-50 relative rounded-md border-2 transition-all ${
+					filterResults ? 'bg-blue-500 border-transparent' : ' border-zinc-500'
+				}`}
+			>
+				{#if filterResults}
+					<i
+						transition:scale
+						class="bi bi-check text-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+					/>
+				{/if}
+			</div>
+		</button>
 	</div>
 </div>
 <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-2 p-4 pt-0">
